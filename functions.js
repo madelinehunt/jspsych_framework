c6f876a0fc17827c6f5697682a558abb629f3eb6
Nathan Hunt <n@thanhunt.com>
2019-04-01 14:34:23 -0400

    minor reorganizing

--- functions.js
+++ functions.js
@@ -1,3 +1,157 @@
+//// functions to help in constructing the timeline
+function copy_default(default_obj){
+  // $.extend( true, {}, a ); // another way of cloning an obj
+  return Object.assign({},default_obj);
+}
+
+function containerize(section, backup=true){
+  if (!$.isArray(section)) {
+    var type = section['type'];
+    var timeline = [section];
+  } else {
+    var type = section[0]['type'];
+    var timeline = section;
+  }
+
+  if (backup){
+
+    for (var i = 0; i < timeline.length; i++) {
+      var cur_params = timeline[i];
+      var backup = JSON.stringify(cur_params);
+      if (typeof(cur_params['data'])!= 'undefined') {
+        data = cur_params['data'];
+      } else {
+        data = {};
+      }
+      data['plugin_parameters_backup'] = backup;
+      timeline[i]['data'] = data;
+    }
+
+    var container = {
+      type: type,
+      timeline: timeline,
+      data: data,
+    };
+
+  } else {
+    var container = {
+      type: type,
+      timeline: timeline,
+    };
+  }
+
+  return container
+}
+
+function init_experiment(){
+  // this depends on lots of global variables, so make sure they exist
+  if (!urlvars.testing) { // defines main (ie non-testing) mode
+    var testing = false;
+    if (capture_partial_data){
+      $(window).on('beforeunload', preserve_partial_data);
+    }
+    jsPsych.init({
+      timeline: define_full_timeline(),
+      on_finish: function(){
+        save_data_and_debrief(log_to_db);
+      },
+    });
+  } else { // defines testing mode
+    var testing = true;
+
+    if (urlvars.timeline) { // helpful if tester types 'timelines' instead of 'timeline'
+      urlvars.timelines = urlvars.timeline;
+    };
+
+    if (urlvars.timelines) {
+      var t_array = urlvars.timelines.split(',');
+      jsPsych.init({
+        timeline: define_testing_timeline(t_array),
+        on_finish: function() {
+          jsPsych.data.displayData();
+        }
+      });
+    } else { // if no 'timeline' urlvar in testing mode, do full timeline
+      jsPsych.init({
+        timeline: define_full_timeline(),
+        on_finish: function() {
+          jsPsych.data.displayData();
+        }
+      });
+    }
+  }
+};
+
+//// I/O functions
+function preserve_partial_data(e){
+  e.preventDefault();
+  var data = jsPsych.data.get().readOnly().csv();
+
+  var params = {
+    subjid: subjID,
+    studyName: study,
+    folder: 'partial_data',
+    name: '_partial_data',
+    toWrite: data,
+  }
+  $.post("/scripts/latest/save.php", params);
+
+  return "If you navigate away from this page, you will lose your progress and you may not get paid. Are you sure you want to proceed?";
+}
+
+function save_data_and_debrief(log_to_db) {
+  if (log_to_db){
+    console.log(study);
+    console.log(condition);
+    console.log(cb_id);
+    updateCBDB(study, condition, cb_id); // this function is defined in counterbalance.js
+  } else {
+    console.log('not logging to DB');
+  }
+  if (response_file_suffix) {
+    var r_file_sfix = response_file_suffix;
+  } else {
+    var r_file_sfix = '_response';
+  }
+
+  var data = jsPsych.data.get().readOnly().csv();
+  var params = {
+    subjid: subjID,
+    studyName: study,
+    name: r_file_sfix,
+    toWrite: data,
+  }
+  $.post("/scripts/latest/save.php", params);
+
+  $('#jspsych-content').append(
+    $('<h2>', {
+      text: 'Please enter the following code into Mechanical Turk:',
+    })
+  ).append($('<h1>', {
+    text: subjID
+  }).css({
+    'background-color': 'rgb(255,225,200)'
+  }));
+
+  var db_obj = {
+    mTurkID: mTurkID,
+    subjID: subjID,
+    study: study,
+    // gender: gend,
+    // political_affiliation: poli
+  };
+
+  if (collect_gender) {
+    db_obj['gender'] = gend;
+  }
+
+  if (collect_pol) {
+    db_obj['political_affiliation'] = poli;
+  }
+
+  $.get('/scripts/latest/log_turker.php', db_obj);
+};
+
 //// misc functions
 function get_randint(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -117,90 +271,6 @@ function jspsych_data_filter(filter_function){
   return filt;
 }
 
-//// functions to help in constructing the timeline
-function copy_default(default_obj){
-  // $.extend( true, {}, a ); // another way of cloning an obj
-  return Object.assign({},default_obj);
-}
-
-function containerize(section, backup=true){
-  if (!$.isArray(section)) {
-    var type = section['type'];
-    var timeline = [section];
-  } else {
-    var type = section[0]['type'];
-    var timeline = section;
-  }
-
-  if (backup){
-
-    for (var i = 0; i < timeline.length; i++) {
-      var cur_params = timeline[i];
-      var backup = JSON.stringify(cur_params);
-      if (typeof(cur_params['data'])!= 'undefined') {
-        data = cur_params['data'];
-      } else {
-        data = {};
-      }
-      data['plugin_parameters_backup'] = backup;
-      timeline[i]['data'] = data;
-    }
-
-    var container = {
-      type: type,
-      timeline: timeline,
-      data: data,
-    };
-
-  } else {
-    var container = {
-      type: type,
-      timeline: timeline,
-    };
-  }
-
-  return container
-}
-
-function init_experiment(){
-  // this depends on lots of global variables, so make sure they exist
-  if (!urlvars.testing) { // defines main (ie non-testing) mode
-    var testing = false;
-    if (capture_partial_data){
-      $(window).on('beforeunload', preserve_partial_data);
-    }
-    jsPsych.init({
-      timeline: define_full_timeline(),
-      on_finish: function(){
-        save_data_and_debrief(log_to_db);
-      },
-    });
-  } else { // defines testing mode
-    var testing = true;
-
-    if (urlvars.timeline) { // helpful if tester types 'timelines' instead of 'timeline'
-      urlvars.timelines = urlvars.timeline;
-    };
-
-    if (urlvars.timelines) {
-      var t_array = urlvars.timelines.split(',');
-      jsPsych.init({
-        timeline: define_testing_timeline(t_array),
-        on_finish: function() {
-          jsPsych.data.displayData();
-        }
-      });
-    } else { // if no 'timeline' urlvar in testing mode, do full timeline
-      jsPsych.init({
-        timeline: define_full_timeline(),
-        on_finish: function() {
-          jsPsych.data.displayData();
-        }
-      });
-    }
-  }
-};
-
 //// counterbalancing
 function local_counterbalancing(conds){
   var randomized_localhost_cond = jsPsych.randomization.sampleWithoutReplacement(conds,1)[0];
@@ -211,74 +281,4 @@ function local_counterbalancing(conds){
     ids: 5444,
   };
   return condition_obj;
-};
-
-//// I/O functions
-function preserve_partial_data(e){
-  e.preventDefault();
-  var data = jsPsych.data.get().readOnly().csv();
-
-  var params = {
-    subjid: subjID,
-    studyName: study,
-    folder: 'partial_data',
-    name: '_partial_data',
-    toWrite: data,
-  }
-  $.post("/scripts/latest/save.php", params);
-
-  return "If you navigate away from this page, you will lose your progress and you may not get paid. Are you sure you want to proceed?";
-}
-
-function save_data_and_debrief(log_to_db) {
-  if (log_to_db){
-    console.log(study);
-    console.log(condition);
-    console.log(cb_id);
-    updateCBDB(study, condition, cb_id); // this function is defined in counterbalance.js
-  } else {
-    console.log('not logging to DB');
-  }
-  if (response_file_suffix) {
-    var r_file_sfix = response_file_suffix;
-  } else {
-    var r_file_sfix = '_response';
-  }
-
-  var data = jsPsych.data.get().readOnly().csv();
-  var params = {
-    subjid: subjID,
-    studyName: study,
-    name: r_file_sfix,
-    toWrite: data,
-  }
-  $.post("/scripts/latest/save.php", params);
-
-  $('#jspsych-content').append(
-    $('<h2>', {
-      text: 'Please enter the following code into Mechanical Turk:',
-    })
-  ).append($('<h1>', {
-    text: subjID
-  }).css({
-    'background-color': 'rgb(255,225,200)'
-  }));
-
-  var db_obj = {
-    mTurkID: mTurkID,
-    subjID: subjID,
-    study: study,
-    // gender: gend,
-    // political_affiliation: poli
-  };
-
-  if (collect_gender) {
-    db_obj['gender'] = gend;
-  }
-
-  if (collect_pol) {
-    db_obj['political_affiliation'] = poli;
-  }
-
-  $.get('/scripts/latest/log_turker.php', db_obj);
 };
